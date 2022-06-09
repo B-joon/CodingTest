7f8577b2d91547aa9271b56785c843e366325c73
B-joon <dosel45@gmail.com>
2022-06-09 18:05:06 +0900

    백준 입출력과 사칙연산 10926번 ??! 문제

---
+++ Beakjoon/src/test1/BJ10926.java
@@ -0,0 +1,24 @@
+package test1;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * 백준 입출력과 사칙연산
+ * 10926번
+ * ??! 문제
+ *
+ */
+
+public class BJ10926 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String id = sc.next();
+		
+		System.out.println(id + "??!");
+		
+	}
+	
+}
