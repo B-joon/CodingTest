fefb3b18e440810a0db104d40b1924604744c559
B-joon <dosel45@gmail.com>
2022-06-10 18:07:58 +0900

    1998년생인 내가 태국에서는 2541년생?!

---
+++ Beakjoon/src/test1/BJ18108.java
@@ -0,0 +1,23 @@
+package test1;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * 백준 입출력과 사칙연산
+ * 18108번
+ * 1998년생인 내가 태국에서는 2541년생?!
+ * 
+ */
+
+public class BJ18108 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		System.out.println(t - 543);
+		
+	}
+}
